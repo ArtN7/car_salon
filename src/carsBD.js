b3114ab06e58862efeb9738ca50b3d95829c0264
ArtN7 <nam_artem_2004@mail.ru>
2024-02-08 18:52:59 +0300

    Load rep from laptop

---
+++ src/carsBD.js
@@ -0,0 +1,47 @@
+const cars = [
+    {
+      "id": 1,
+      "name": "A",
+      "url": "https://www.hdcarwallpapers.com/thumbs/2024/mclaren_artura-t2.jpg",
+      "alt": "Radiation resistance",
+      "description": "description description description description description description description",
+      "year": 2016,
+      "mileage": 120000,
+      "hp": 670,
+      "price": 23000000
+    },
+    {
+      "id": 2,
+      "name": "B",
+      "url": "https://www.hdcarwallpapers.com/thumbs/2024/mclaren_artura-t2.jpg",
+      "alt": "Radiation resistance",
+      "description": "description",
+      "year": 2016,
+      "mileage": 120000,
+      "hp": 670,
+      "price": 450000
+    },
+    {
+      "id": 3,
+      "name": "C",
+      "url": "https://www.hdcarwallpapers.com/thumbs/2024/mclaren_artura-t2.jpg",
+      "alt": "Radiation resistance",
+      "description": "description description description description description description description",
+      "year": 2016,
+      "mileage": 120000,
+      "hp": 670,
+      "price": 450000
+    },
+    {
+      "id": 4,
+      "name": "D",
+      "url": "https://www.hdcarwallpapers.com/thumbs/2024/mclaren_artura-t2.jpg",
+      "alt": "Radiation resistance",
+      "description": "description",
+      "year": 2016,
+      "mileage": 120000,
+      "hp": 670,
+      "price": 450000
+    },
+  ]
+export default cars;
---
+++ src/components/screens/carDetails/carDetail/CarDetail.jsx
@@ -0,0 +1,37 @@
+import {useNavigate, useParams } from "react-router-dom";
+import cars from "../../../../carsBD.js"
+import styles from "./CarDetail.module.css"
+import CarDetailDescription from "../carDetailDescription/CarDetailDescription.jsx";
+
+const CarDetail = () => {
+    const nav = useNavigate();
+    const { id } = useParams();
+    for (let i = 0; i < cars.length; i += 1){
+        if (Number(id) === cars[i].id) {
+            return (
+                <div className={styles.carDetail}>
+                    <div className={styles.image} alt={cars[i].name} style={{
+                        backgroundImage:`url(${cars[i].url})`,
+                        }}>
+                        <button onClick={()=> nav('/')} className='btn btnBack'>Back</button>
+                    </div>
+                    <div className={styles.miniInfo}>
+                        <h2>{cars[i].name}</h2>    
+                        <p>{new Intl.NumberFormat('en-US', {
+                            style: 'currency', currency: 'USD',
+                        }).format(cars[i].price)}</p>
+                        <p>{cars[i].description}</p>
+                        <div>
+                            <CarDetailDescription car={cars[i]}/>
+                        </div>
+                    </div>
+                </div>
+
+            )
+        }
+    }
+    return (
+        <div className = {styles.error}>"Not found or insufficient data"</div>               
+    )
+}
+export default CarDetail;
