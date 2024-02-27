11c77a6b3d51f6a934b256787e019a9e929a828f
ArtN7 <nam_artem_2004@mail.ru>
2024-02-27 18:42:41 +0300

    Upgrade promotion componet. add validation

--- src/components/screens/main/carPromotion/CarPromotion.jsx
+++ src/components/screens/main/carPromotion/CarPromotion.jsx
@@ -1,52 +1,46 @@
 import { number, object, string } from 'yup'
 import styles from './CarPromotion.module.css'
-import { useState } from 'react'
-
-let userSchema = object({
-    userName: string().required(),
-    phone: number().required(),
-    email: string().email().required()
-});
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const CarPromotion = () => {
-    const [isError, setError] = useState(true);
-    const [form, setForm] = useState({
-        userName: '',
-        phone: '',
-        email: ''
-    })
-    const handleChange = (e) => {
-        const {name, value} = e.target; 
-        setForm({
-            ...form,
-            [name]: value,
-        })
-    }
-    const handleButtonClick = (e) => {
-        e.preventDefault()
-        setError(userSchema.isValidSync(form));
-        const inputs = document.getElementById('promotionForm')?.querySelectorAll('input');
-        inputs.forEach(input => input.value = '');
-        setForm({
-            userName: '',
+    const {register, handleSubmit, formState: { errors }} = useForm({
+        defaultValues: {
+            name: '',
             phone: '',
             email: ''
-        });
-        // const resultOfForm = JSON.stringify(form);
+        }
+    });
+    const [isFormValid, setIsFormValid] = useState(false);
+    const onSubmit = (data) =>  {
+        setIsFormValid(true);
+        console.log(data)
     }
     return (
         <div className={styles.container}>
-            <div>
+            <div className={styles.promoContainer}>
                 <h2>Акция</h2>
                 <p className={styles.description}>При онлайн оформлении заявки на покупку автомобиля комплект зимней резины в подарок</p>
             </div>
-            <p>Оставляя заявку, вы соглашаетесь на обработку персональных данных</p>
-            <form id={'promotionForm'} className={styles.form} onSubmit={(e) => handleButtonClick(e)}>
-                <input name={'userName'} placeholder='Имя' onChange={handleChange}/>
-                <input name={'phone'} placeholder='Телефон' onChange={handleChange}/>
-                <input name={'email'} placeholder='Почта' onChange={handleChange}/>
-                {isError ? null : <p>Введите корректные значения. Поля не должны быть пустыми</p>}
-                <button type='submit'>Отправить</button>
+            <p className={styles.personalData}>Оставляя заявку, вы соглашаетесь на обработку персональных данных</p>
+            <form id={'promotionForm'} className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    Имя
+                    <input {...register('name', {required: 'Заполните поле'})} type='text' placeholder='Имя'/>
+                    <p>{errors.name?.message}</p>
+                </label>
+                <label>
+                    Телефон
+                    <input {...register('phone', {required: 'Заполните поле',  pattern: {value: /^\(?([0-9]{1})?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, message:'Введите корректный номер(содержит 11 цифр)'}})} type='text' placeholder='Телефон' />
+                    <p>{errors.phone?.message}</p>
+                </label>
+                <label>
+                    Почта
+                    <input {...register('email', {required: 'Заполните поле', pattern: {value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, message:'Введите корректные имейл'}})} type='text' placeholder='Почта' />
+                    <p>{errors.email?.message}</p>
+                </label>
+                <button type='submit' className='btn'>Отправить</button>
+                {isFormValid ? <p>Отправлено</p>: ''}
             </form>
         </div>
     )
