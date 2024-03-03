f18c7972598185294e02787d2451b72667082ac9
ArtN7 <nam_artem_2004@mail.ru>
2024-03-03 16:15:20 +0300

    upd styles for company module

--- src/components/screens/main/AboutCompany/AboutCompany.jsx
+++ src/components/screens/main/AboutCompany/AboutCompany.jsx
@@ -5,16 +5,18 @@ const AboutCompany = () => {
         <div className={styles.container}>
             <h2>О компании</h2>
             <div className={styles.infoCompany}>
-                <p>text</p>
+                <p>Мы возглавляем топ-3 автосалонов по количеству продаж. Наша компания продает автомобили более 20 лет на рынке. 
+                    text text text
+                </p>
                 <a className='link' href={`/car_salon_3kurs/aboutcompany`}>Больше информации</a>
             </div>
             <div className={styles.trust}>
-                <p>Почему нам доверяют?</p>
+                <h3>Почему нам доверяют?</h3>
                 <ul className={styles.facts}>
-                    <li className={styles.fact}>Мы возглавляем топ-3 автосалонов по количеству продаж</li>
+                    <li className={styles.fact}>Продано более 12000 автомобилей.</li>
                     <li className={styles.fact}>У нас самый большой выбор</li>
                     <li className={styles.fact}>Самые низкие процентные ставки по автокредитам</li>
-                    <li className={styles.fact}>бесплатное ТО в течение 2-х лет после покупки нового автомобиля</li>
+                    <li className={styles.fact}>Бесплатное ТО в течение 2-х лет после покупки</li>
                 </ul>
             </div>
         </div>
