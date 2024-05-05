0d717ca70078215141991dddfb80a8ced7d211cd
ArtN7 <nam_artem_2004@mail.ru>
2024-05-05 20:57:29 +0300

    change logic filters, search, sorts. Now it is working

--- src/components/screens/carsCatalog/CarsCatalog.jsx
+++ src/components/screens/carsCatalog/CarsCatalog.jsx
@@ -6,59 +6,60 @@ import CarFilters from './carFilters/CarFilters'
 import styles from '../carsCatalog/CarsCatalog.module.css'
 import SearchBar from '../../UI/searchBar/SearchBar';
 
-let searchCars = [];
-let filteredCars = [];
 const CarsCatalog = ({cars}) => {
     const nav = useNavigate();
     const location = useLocation();
     const nameCategory = location.pathname.split('/').at(-1);
     cars = Object.values(cars)[0]; //this str will be delete when i create json server
     const newCars = cars.filter(car => car.category === nameCategory);
-    searchCars = newCars;
-    filteredCars = newCars;
     const [allCars, setAllCars] = useState(newCars);
-    
-    const handleSearch = (e) => {
+    let searchCars = newCars;
+    const handleSearchLogic = (e) => {
         e.preventDefault();
-        filteredCars = searchCars;
         const inputValue = document.getElementById('inputSearch').value;
-        const filtered = filteredCars.filter(car =>
+        const filtered = newCars.filter(car =>
             car.brand.toLowerCase().includes(inputValue.toLowerCase()) ||
             car.model.toLowerCase().includes(inputValue.toLowerCase())
         );
-        searchCars = filtered.map(item2 => filteredCars.filter(item1 => item1.id === item2.id)).flat();
-        setAllCars(searchCars);
+        searchCars = filtered;
+        return filtered;
     };
-    const handleSearchReset = (e) => {
-        e.preventDefault();
-        document.getElementById('inputSearch').value = '';
-        handleFilter(e);
-    }
-    const handleFilter = (e) => {
+    const handleFilterLogic = (e) => {
         e.preventDefault();
         const partInputs = document.querySelectorAll('#filterPartInputsContainer label input');
-        // handleResetFilter(e)
+        let filtered = [];
         for (let i = 0; i < partInputs.length - 1; i += 2 ) {
             const nameInput = partInputs[i].name.toLowerCase();
             let maxValue = partInputs[i + 1].value === '' ? 10000000000 : Number(partInputs[i + 1].value);
-            filteredCars = filteredCars.filter((car) => {
+            filtered = newCars.filter((car) => {
                 return car[nameInput] >= Number(partInputs[i].value) && car[nameInput] <= maxValue;
             });
         }
-        setAllCars([...filteredCars])
+        return filtered;
     }
+    const handleFiltersAll = (e) => {
+        e.preventDefault();
+        setAllCars([...handleFilterLogic(e).map(item2 => handleSearchLogic(e).filter(item1 => item1.id === item2.id)).flat()]);
+    }
+    const handleSearchReset = (e) => {
+        e.preventDefault();
+        setAllCars([...handleFilterLogic(e)]);
+        searchCars = allCars;
+        const input = document.getElementById("inputSearch");
+        input.value = '';
+    }
+
     const handleResetFilter = (e) => {
         e.preventDefault();
-        const input = document.getElementById("inputSearch");
-        console.log(input.value)
-        if (input.value === '') setAllCars(newCars);
-        else handleSearch(e)
-        // if (partInputs !== undefined){
-        //     partInputs.forEach((input) => {
-        //         if (input.value !== '') input.value = '';
-        //     });
-        // }
+        setAllCars([...handleSearchLogic(e)]);
+        const partInputs = document.querySelectorAll('#filterPartInputsContainer label input');
+        if (partInputs !== undefined){
+            partInputs.forEach((input) => {
+                if (input.value !== '') input.value = '';
+            });
+        }
     }
+
 
     const filterInputs = (filter_attributes) => {
         return filter_attributes.reduce((acc, currentValue, index) => {
@@ -80,11 +81,11 @@ const CarsCatalog = ({cars}) => {
             <div id={'carsCatalogContainer'} className={styles.container}>
                 <button className={buttonClass} onClick={() => nav('/car_salon_3kurs')}>Назад</button>
                 <div className={styles.containerSortsFilters}>
-                    <Sorts cars={allCars}/>
-                    <CarFilters cars={allCars} setAllCars={setAllCars} filterInputs={filterInputs} handleFilter={handleFilter} handleResetFilter={handleResetFilter}/>
+                    <Sorts cars={allCars} setAllCars={setAllCars}/>
+                    <CarFilters cars={allCars} filterInputs={filterInputs} handleFilter={handleFiltersAll} handleResetFilter={handleResetFilter}/>
                 </div>
                 <div className={styles.containerSearchCars}>
-                    <SearchBar handleSearch={handleSearch} handleReset={handleSearchReset}/>
+                    <SearchBar handleSearch={handleFiltersAll} handleReset={handleSearchReset}/>
                     <ul className={styles.catalogs}>
                         {allCars.length > 0 ? 
                             allCars.map((car) => {
